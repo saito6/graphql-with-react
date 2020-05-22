d9699dff69cbcbe0e796df27c65246aa5f55ffff
saito6 <Kingdomsaito@icloud.com>
2020-05-23 00:47:36 +0900

    setup graphql-client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import client from './client';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import {ApolloProvider} from 'react-apollo';
 
-function App() {
+
+// ===========================
+
+const ME = gql`
+  query me{
+    user(login: "iteachonudemy"){
+      name
+      avatarUrl
+    }
+  }
+`
+
+const App = () => {
   return (
-    <div>Hello</div>
+    <ApolloProvider client={client}>
+      <div>Hello</div>
+      <Query query={ME}>
+        {
+          ({loading,error,data}) => {
+            if(loading) return 'Loading...'
+            if(error) return `Error! ${error.message}`
+            return <div>{data.user.name}</div>
+          }
+        }
+      </Query>
+    </ApolloProvider>
   );
 }
 
