16442279b0af3b8b09e9770b8f2ac1da7a035c65
saito6 <Kingdomsaito@icloud.com>
2020-05-30 18:02:30 +0900

    titlize

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import client from './client';
-import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import {ApolloProvider} from 'react-apollo';
-
+import { ME } from './graphql';
+import { SEARCH_REPOSITORIES } from './graphql';
 
 // ===========================
 
-const ME = gql`
-  query me{
-    user(login: "iteachonudemy"){
-      name
-      avatarUrl
-    }
-  }
-`
 
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <div>Hello</div>
-      <Query query={ME}>
-        {
-          ({loading,error,data}) => {
-            if(loading) return 'Loading...'
-            if(error) return `Error! ${error.message}`
-            return <div>{data.user.name}</div>
-          }
+const DEFAULT_STATE = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = DEFAULT_STATE;
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    })
+  }
+
+  render(){
+    const { query, first, last, before, after } = this.state;
+    console.log({query});
+    return (
+      <ApolloProvider client={client}>
+      <form onSubmit={this.handleSubmit}>
+        <input value={query} onChange={this.handleChange}></input>
+      </form>
+      <Query
+      query={SEARCH_REPOSITORIES}
+      variables={{ query, first, last, before, after }}
+      >
+      {
+        ({loading,error,data}) => {
+          if(loading) return 'Loading...'
+          if(error) return `Error! ${error.message}`
+          const search = data.search;
+          const repositoryCount = search.repositoryCount;
+          const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
+          const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
+          return <h2>{title}</h2>
         }
+      }
       </Query>
-    </ApolloProvider>
-  );
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
