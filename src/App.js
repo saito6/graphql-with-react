bd33630ac087565025ea47a612dd461ba6b539ac
saito6 <Kingdomsaito@icloud.com>
2020-06-20 19:04:16 +0900

    next button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React,{ Component } from 'react';
-import client from './client';
-import { Query } from 'react-apollo';
-import {ApolloProvider} from 'react-apollo';
-import { ME } from './graphql';
-import { SEARCH_REPOSITORIES } from './graphql';
+import React, { Component } from 'react'
+import { ApolloProvider } from 'react-apollo'
+import { Query } from 'react-apollo'
+import client from './client'
+import { SEARCH_REPOSITORIES } from './graphql'
 
-// ===========================
-
-
+const PER_PAGE = 5
 const DEFAULT_STATE = {
-  first: 5,
+  first: PER_PAGE,
   after: null,
   last: null,
   before: null,
@@ -17,63 +14,91 @@ const DEFAULT_STATE = {
 }
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = DEFAULT_STATE;
-    this.handleChange = this.handleChange.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = DEFAULT_STATE
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange(event){
+  handleChange(event) {
     this.setState({
       ...DEFAULT_STATE,
       query: event.target.value
     })
   }
 
-  render(){
-    const { query, first, last, before, after } = this.state;
-    console.log({query});
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null
+    })
+  }
+
+  render() {
+    const { query, first, last, before, after } = this.state
+    console.log({query})
+
     return (
       <ApolloProvider client={client}>
-      <form onSubmit={this.handleSubmit}>
-        <input value={query} onChange={this.handleChange}></input>
-      </form>
-      <Query
-      query={SEARCH_REPOSITORIES}
-      variables={{ query, first, last, before, after }}
-      >
-      {
-        ({loading,error,data}) => {
-          if(loading) return 'Loading...'
-          if(error) return `Error! ${error.message}`
-          const search = data.search;
-          const repositoryCount = search.repositoryCount;
-          const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
-          const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`;
-          return (
-            <>
-              <h2>{title}</h2>
-              <ul>
-                {
-                  search.edges.map(edge => {
-                    const node = edge.node;
+        <form onSubmit={this.handleSubmit}>
+          <input value={query} onChange={this.handleChange} />
+        </form>
+        <Query
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
+          {
+            ({ loading, error, data }) => {
+              if (loading) return 'Loading...'
+              if (error) return `Error! ${error.message}`
 
-                    return(
-                      <li key={node.id}>
-                        <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
-                      </li>
-                    );
-                  })
-                }
-              </ul>
-            </>
-          );
-        }
-      }
-      </Query>
+              const search = data.search
+              const repositoryCount = search.repositoryCount
+              const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
+              const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
+              return (
+                <React.Fragment>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      search.edges.map(edge => {
+                        const node = edge.node
+
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                      <button
+                        onClick={this.goNext.bind(this, search)}
+                      >
+                        Next
+                      </button>
+                      :
+                      null
+                  }
+                </React.Fragment>
+              )
+            }
+          }
+        </Query>
       </ApolloProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
